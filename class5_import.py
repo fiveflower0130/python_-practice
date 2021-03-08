689d40086e534277cb606236e623a611e39198c3
fiveflower0130 <fiveflower0130@gmail.com>
2021-03-08 17:10:43 +0800

    update code

--- class5_import.py
+++ class5_import.py
@@ -1,17 +1,30 @@
 import random
 
-r = random.randint(1, 100)
+while True:
+    start = int(input("please input your start value: "))
+    end = int(input("please input your end value: "))
+    if start <= 0 or start > end:
+        print("incorrect value for start, please try again!")
+    elif end <= 0 or end < start:
+        print("incorrect value for end, please try again!")
+    else:
+        break
+r = random.randint(start, end)
 count = 0
 while True:
-    count += 1
-    a = int(input('please input a number between 1 - 100: '))
-    if a == r:
-        print(f'answer is {r}, you got it!')
-        print(f'You have guess {count} times!')
-        break
-    elif a > r:
-        print('Your number is higher than answer!')
-    elif a < r:
-        print('Your number is lower than answer!')
+    a = int(input(f'please input a number between {start} - {end}: '))
+    if a >= start and a <= end:
+        count += 1
+        if a == r:
+            print(f'answer is {r}, you got it!')
+            print(f'You have guess {count} times!')
+            break
+        elif a > r:
+            print('Your number is higher than answer!')
+        elif a < r:
+            print('Your number is lower than answer!')
+        else:
+            print('something is wrong!')
     else:
-        print('something is wrong!')
+        print(
+            f'The number is wrong, it should between {start} - {end} , please retry again')
