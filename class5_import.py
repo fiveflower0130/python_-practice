4488332566a7bb980711d34572755436f5140927
fiveflower0130 <fiveflower0130@gmail.com>
2021-03-08 16:41:05 +0800

    add new version

--- class5_import.py
+++ class5_import.py
@@ -1,10 +1,13 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     a = int(input('please input a number between 1 - 100: '))
     if a == r:
-        print(f'r = {r}, you got it!')
+        print(f'answer is {r}, you got it!')
+        print(f'You have guess {count} times!')
         break
     elif a > r:
         print('Your number is higher than answer!')
